5d9bfbeae65ca93ccbd24ec29797be05ca057626
Anurag Kumar <pumbatimon473@gmail.com>
2024-05-14 21:20:36 +0530

    Feature 1: Rectangle

---
+++ rectangle.py
@@ -0,0 +1,9 @@
+"""Feature 1: Rectangle"""
+
+
+def area(length: float, breadth: float) -> float:
+    return length * breadth
+
+
+def perimeter(length: float, breadth: float) -> float:
+    return 2 * (length + breadth)
